c790776cb90b0c53ece55b3cff59057f6314d0cf
David <davidmjg17@gmail.com>
2024-09-12 09:46:04 +0100

    Cascade Drop Down List

--- SuperShop/Data/ICountryRepository.cs
+++ SuperShop/Data/ICountryRepository.cs
@@ -1,5 +1,7 @@
-﻿using SuperShop.Data.Entities;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using SuperShop.Data.Entities;
 using SuperShop.Models;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -19,7 +21,12 @@ namespace SuperShop.Data
         Task<int> UpdateCityAsync(City city);
 
         Task<int> DeleteCityAsync(City city);
-       
+
+        IEnumerable<SelectListItem> GetComboCountries();
+
+        IEnumerable<SelectListItem> GetComboCities(int countryId);
+
+        Task<Country> GetCountryAsync(City city);
 
     }
 }
