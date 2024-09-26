3fc6d64faab5d4112cd1ea20b639ac2ce896f148
David <davidmjg17@gmail.com>
2024-09-26 16:07:08 +0100

    MultiLanguages

--- SuperShop.Prism/SuperShop.Prism/ViewModels/ProductsPageViewModel.cs
+++ SuperShop.Prism/SuperShop.Prism/ViewModels/ProductsPageViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
+using SuperShop.Prism.Helpers;
 using SuperShop.Prism.ItemViewModels;
 using SuperShop.Prism.Models;
 using SuperShop.Prism.Services;
@@ -27,7 +28,7 @@ namespace SuperShop.Prism.ViewModels
         {
             _navigationService = navigationService;
             _apiService = apiService;
-            Title = "Products Page";
+            Title = Languages.Products;
             LoadProductsAsync();
         }
 
@@ -61,7 +62,7 @@ namespace SuperShop.Prism.ViewModels
             {
                 Device.BeginInvokeOnMainThread(async () =>
                 {
-                    await App.Current.MainPage.DisplayAlert("Error", "Check Internet Connection", "Accept");
+                    await App.Current.MainPage.DisplayAlert(Languages.Error, Languages.ConnectionError, Languages.Accept);
                 });
                
                 return;
@@ -78,7 +79,7 @@ namespace SuperShop.Prism.ViewModels
 
             if (!response.IsSuccess)
             {
-                await App.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
+                await App.Current.MainPage.DisplayAlert(Languages.Error, response.Message, Languages.Accept);
                 return;
             }
 
